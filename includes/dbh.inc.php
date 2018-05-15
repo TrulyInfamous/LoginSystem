e1b0cd91fb086d76f67426548f123639d9fbd22b
Scott Young <you@example.com>
2018-05-15 17:56:01 +1200

    My first login system

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPasswordname = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPasswordname, $dbName);
